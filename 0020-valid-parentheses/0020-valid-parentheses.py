4d7178ad70a27f3a6015b8062ae207db3c36f229
Rizouana Prome <154711860+rprome@users.noreply.github.com>
2025-10-02 23:50:31 -0400

    Time: 6 ms (18.23%), Space: 12.6 MB (28.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping: 
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:  
+                stack.append(char)
+
+        return not stack
