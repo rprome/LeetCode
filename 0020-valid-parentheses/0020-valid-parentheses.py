bf9c148c73865db811fad53c7308530e2fc843cd
Rizouana Prome <154711860+rprome@users.noreply.github.com>
2025-10-02 23:59:39 -0400

    Time: 0 ms (100%), Space: 12.6 MB (61.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,12 +4,11 @@ class Solution(object):
         mapping = {')': '(', '}': '{', ']': '['}
 
         for char in s:
-            if char in mapping: 
+            if char in mapping:
                 if stack and stack[-1] == mapping[char]:
                     stack.pop()
                 else:
                     return False
-            else:  
+            else:
                 stack.append(char)
-
-        return not stack
+        return not stack 
